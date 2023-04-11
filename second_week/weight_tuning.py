04ef4cb1ba8f6688e1daa18e26cef2165618311c
johnnychen <johnny861110@gmail.com>
2023-04-11 19:10:22 +0800

    update

--- second_week/weight_tuning.py
+++ second_week/weight_tuning.py
@@ -4,6 +4,8 @@ class WeightTuning():
     def __init__(self, learning_rate):
         self.learning_rate = learning_rate
         
+    
+    
     def update_weights_LG(self, model, inputs, targets, category_threshold):
         optimizer = tf.keras.optimizers.SGD(learning_rate=self.learning_rate)
         for epoch in range(category_threshold):
@@ -12,6 +14,37 @@ class WeightTuning():
                 loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)(targets, predictions)
             gradients = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    
+    
+    
+    def update_weights_LG_UA(self, model, inputs, targets,category_threshold,ua_threshold):
+        optimizer = tf.keras.optimizers.SGD(learning_rate=self.learning_rate)
+        for epoch in range(category_threshold):
+            with tf.GradientTape() as tape:
+                predictions = model(inputs)
+                loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)(targets, predictions)
+                
+            gradients = tape.gradient(loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+            
+            # Check if learning rate is less than ua_threshold
+            if self.learning_rate <= ua_threshold:
+                return
+            
+            # Check if previous loss is greater than current loss
+            if prev_loss is not None and loss >= prev_loss:
+                self.learning_rate = self.learning_rate * 1.2
+                for i, w in enumerate(model.trainable_variables):
+                    w.assign(weight_backup[i])
+                model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True), optimizer=optimizer)
+                return self.update_weights_LG_UA(model, inputs, targets,ua_threshold)
+            
+            prev_loss = loss
+            weight_backup = [w.numpy() for w in model.trainable_variables]
+            self.learning_rate = self.learning_rate * 0.7
+            model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True), optimizer=optimizer)
+            
+    
     
     def update_weights_EB(self, model, inputs, targets, num_epochs, batch_size):
         optimizer = tf.keras.optimizers.SGD(learning_rate=self.learning_rate)
@@ -24,6 +57,8 @@ class WeightTuning():
                     loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)(target_batch, predictions)
                 gradients = tape.gradient(loss, model.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    
+    
     
     def update_weights_EB_LG(self, model, inputs, targets, num_epochs, batch_size, category_threshold):
         optimizer = tf.keras.optimizers.SGD(learning_rate=self.learning_rate)
@@ -39,3 +74,34 @@ class WeightTuning():
                 if epoch > category_threshold and loss >= prev_loss:
                     return
                 prev_loss = loss
+    
+    
+            
+    def update_weights_EB_LG_UA(self, model, inputs, targets, num_epochs, batch_size, category_threshold,ua_threshold):
+        optimizer = tf.keras.optimizers.SGD(learning_rate=self.learning_rate)
+        prev_loss = float('inf')
+        for epoch in range(num_epochs):
+            for i in range(0, len(inputs), batch_size):
+                input_batch = inputs[i:i+batch_size]
+                target_batch = targets[i:i+batch_size]
+                with tf.GradientTape() as tape:
+                    predictions = model(input_batch)
+                    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)(target_batch, predictions)
+                gradients = tape.gradient(loss, model.trainable_variables)
+                if loss >= prev_loss:
+                    self.learning_rate *= 1.2
+                    model.set_weights(prev_weights)
+                    optimizer.learning_rate = self.learning_rate
+                    break
+                else:
+                    prev_weights = model.get_weights()
+                    prev_loss = loss
+                    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+                    if epoch > category_threshold and loss >= prev_loss:
+                        return
+                    if self.learning_rate < ua_threshold:
+                        model.set_weights(prev_weights)
+                        optimizer.learning_rate *= 0.7
+                        break
+
+    
